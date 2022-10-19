25fbf97d582b903c95d93939833330ab6710f546
Gilixx <116208924+Gilixx@users.noreply.github.com>
2022-10-19 13:58:35 -0500

    Add files via upload
    Menu chido

--- Proyecto Integrador 1er Semestre/menu.py
+++ Proyecto Integrador 1er Semestre/menu.py
@@ -1,23 +1,30 @@
+from cProfile import label
+from lib2to3.pgen2.token import LBRACE
+from modulefinder import IMPORT_NAME
+import posixpath
 from tkinter import *
-from PIL import ImageTk, Image
 from tkinter import font
+from PIL import ImageTk, Image
+
 
 root = Tk()
 root.title("Menú Principal")
 root.resizable(FALSE, FALSE)
 root.geometry('1000x700+300+50')
 root.option_add('*tearOff', FALSE)
-# root.configure(background='#6dd5ed')
-# win = Toplevel(root)
-# Menu Bar
-bg_image = ImageTk.PhotoImage(Image.open("images/Background Menu.png"))
-bg_label = Label(root, image= bg_image).pack()
+root.configure(background='cyan')
+#
+my_img = ImageTk.PhotoImage(Image.open('Menu1.png'))
+my_lable = Label(image=my_img)
 
-# menubar = Menu(root, bg='#56ab2f')
+my_lable.pack()
+
+# Menu Bar
+menubar = Menu(root, bg='#56ab2f')
 root['menu'] = menubar
-menu_options = Menu(menubar, bg='#56ab2f', fg='white')
-menu_extra = Menu(menubar, bg='#56ab2f', fg='white')
-menu_tutorial = Menu(menubar, bg='#56ab2f', fg='white')
+menu_options = Menu(menubar, bg='#000000', fg='black')
+menu_extra = Menu(menubar, bg='#000000', fg='black')
+menu_tutorial = Menu(menubar, bg='#000000', fg='black')
 
 # Pestaña Opciones
 menubar.add_cascade(menu=menu_options, label='Opciones')
@@ -31,8 +38,7 @@ menu_tutorial.add_command(label='Preguntas Frecuentes')
 # Pestaña Otros
 menubar.add_cascade(menu=menu_extra, label= 'Otros')
 
-welcome_lbl = Label(root, text="Iniciemos un entrenamiento", font=("Yu Gothic UI Semibold", 30), background='#6dd5ed').pack(pady=10)
-
+welcome_lbl = Label(root, text="Iniciemos un entrenamiento", font=("Yu Gothic UI Semibold", 30), background='cyan').pack(pady=10)
 button_start_training = Button(root, text="Iniciar", width=20, height=5, background='#56ab2f', foreground= 'white', font=("Segoe UI Black", 20), state= DISABLED)
 button_start_training.pack(pady=20)
 
@@ -41,10 +47,9 @@ def choose_training(value):
         button_start_training["state"] = NORMAL
 
 training = IntVar()
-three_minutes = Radiobutton(root, text="3 Minutos", variable=training, value= 1, font=("Yu Gothic", 15), command= lambda: choose_training(training.get()), bg= '#6dd5ed').pack()
-five_minutes = Radiobutton(root, text="5 Minutos", variable=training, value= 2, font=("Yu Gothic", 15), command= lambda: choose_training(training.get()), bg= '#6dd5ed').pack()
-seven_minutes = Radiobutton(root, text="7 Minutos", variable=training, value= 3, font=("Yu Gothic", 15), command= lambda: choose_training(training.get()), bg= '#6dd5ed').pack()
-
+three_minutes = Radiobutton(root, text="3 Minutos", variable=training, value= 1, font=("Yu Gothic", 15), command= lambda: choose_training(training.get()), bg= 'cyan').pack()
+five_minutes = Radiobutton(root, text="5 Minutos", variable=training, value= 2, font=("Yu Gothic", 15), command= lambda: choose_training(training.get()), bg= 'cyan').pack()
+seven_minutes = Radiobutton(root, text="7 Minutos", variable=training, value= 3, font=("Yu Gothic", 15), command= lambda: choose_training(training.get()), bg= 'cyan').pack()
 
 
 
